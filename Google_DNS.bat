2aa7d230836265da532322d8b77c7724d3eb5e52
MoRazif <37921193+MoRazif@users.noreply.github.com>
2022-06-29 22:33:06 +0800

    Add files via upload

---
+++ Google_DNS.bat
@@ -0,0 +1,10 @@
+rem Changing DNS to Google Public DNS; 8.8.8.8, 8.8.4.4
+rem Mohamad Razif Abdul Rahim
+
+netsh interface ipv4 set dns name="Local Area Connection" static 8.8.8.8 primary
+netsh interface ipv4 add dns name="Local Area Connection" 8.8.4.4
+ipconfig /flushdns
+
+echo DNS Changed to Primary : 8.8.8.8 , Secondary 8.8.4.4
+
+pause
